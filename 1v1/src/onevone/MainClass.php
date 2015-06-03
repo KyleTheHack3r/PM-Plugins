e68402bacfd60ee65cc3fe1c3371940ece61fd16
KyleTheHack3r <thepmteam@hotmail.com>
2015-06-03 11:51:01 +0100

    Update MainClass.php

--- 1v1/src/onevone/MainClass.php
+++ 1v1/src/onevone/MainClass.php
@@ -2,9 +2,11 @@
 namespace onevone;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerDeathEvent;
 class MainClass extends PluginBase{
 
 $game = array('game1','game2','game3');
@@ -16,6 +18,14 @@ $game3 = 1;
 // 2 = if set; the request to $request has been sent
 // 3 = $request has accepted (teleport) 
 
+public function onEnable(){
+          $this->getLogger()->info("1v1 Enabled");
+     }
+ 
+/** @var etc */
+private $etc;
+    
+    
  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
         if(strtolower($cmd->getName()) === "1v1") {
             if($sender->hasPermission("onevone.command.go")) {
@@ -23,31 +33,38 @@ $game3 = 1;
                     $request = $this->getServer()->getPlayer($args[0]);
                     if($request instanceof Player) {
                         $game1 = 2;
-                        $request->sendMessage("<1v1> . $sender . " has requested to 1v1 you.");
+                        $request->sendMessage("<1v1> " . $sender . " has requested to 1v1 you.");
                         $request->sendMessage("<1v1> Type '/1v1 accept' to accept the request.");
                         // do some stuff here
-                        /*
                             $random_game = array_rand($game);
                             if($random_game == "game1"){
                                 if($accepted == 1){
+                                    $request->getInventory()->setContents(array(Item::get(0, 0, 0)));
                                     //setPosition (Vector3 $pos)
-                                    $game1 = 3;
+                                    if($death = 1){
+                                     
+                                         $game1 = 3;
+                                    }
                                 }
                                 // close "game1" for other people
                             }elseif($random_game == "game2"){
                                 // do the same stuff as above
-                                if($accepted == 1){
+                                $request->getInventory()->setContents(array(Item::get(0, 0, 0)));
                                     //setPosition (Vector3 $pos)
-                                    $game2 = 3;
+                                    if($death = 1){
+                                     
+                                         $game2 = 3;
+                                    }
                                 }
                             }else($random_game == "game3"){
                                 // and again
-                                if($accepted == 1){
+                                $request->getInventory()->setContents(array(Item::get(0, 0, 0)));
                                     //setPosition (Vector3 $pos)
-                                    $game3 = 3;
-                                }
+                                    if($death = 1){
+                                     
+                                         $game3 = 3;
+                                    }
                             }
-                        */
                     }
                     $sender->sendMessage("<1v1> Your invitation has been sent to " . $request . ".");      
                 }else{
@@ -61,6 +78,13 @@ $game3 = 1;
  }
 
 // respawn and death event functions here
+public function onSpawn(PlayerRespawnEvent $event){
+    // leave anything
+    $death = 1;
+}
+public function onPlayerDeath(PlayerDeathEvent $event){
+ // do something here
+}
 
 
 }
